a419bb497797a46a2663fef9ecc0de24a221c191
root <jose.zapata305@gmail.com>
2019-01-17 21:30:13 -0500

    Updates

---
+++ precache-manifest.7f4f0d4d93247b87a055d7abcb3b55bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81be3e5ad54c032afce0",
+    "url": "/react-homework/static/js/runtime~main.81be3e5a.js"
+  },
+  {
+    "revision": "0c5c1f1e98ff584f96aa",
+    "url": "/react-homework/static/js/main.0c5c1f1e.chunk.js"
+  },
+  {
+    "revision": "57f7ace45ab017c521ad",
+    "url": "/react-homework/static/js/1.57f7ace4.chunk.js"
+  },
+  {
+    "revision": "0c5c1f1e98ff584f96aa",
+    "url": "/react-homework/static/css/main.9c269a8c.chunk.css"
+  },
+  {
+    "revision": "b7f29284fecf7bf3afa49cc18cf5db3a",
+    "url": "/react-homework/index.html"
+  }
+];
